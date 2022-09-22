b8fb82d7dad77234c871d28d81d27adef45a5840
emukus <mukuna.eugene@gmail.com>
2022-09-22 14:37:29 +0000

    Update header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,11 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,9 +23,11 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
+typedef struct binary_tree_s bst_t; /* Binary Search Tree */
+typedef struct binary_tree_s avl_t; /* AVL Tree */
+typedef struct binary_tree_s heap_t; /* Max Binary Heap */
+
+void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
