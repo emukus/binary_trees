315d43b3d9a69284636133cbe3e597cafeef3493
emukus <mukuna.eugene@gmail.com>
2022-09-22 15:43:00 +0000

    Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,7 +9,5 @@
 
 size_t binary_tree_depth(const binary_tree t *tree)
 {
-	if (tree)
-	{
-		return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
-	}
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
