72f9733367955e911ffec33c98042c09503dc1c8
emukus <mukuna.eugene@gmail.com>
2022-09-22 15:41:39 +0000

    Depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: ptr to the node to measure depth
+ *
+ * Return: 0 if tree is NULL, else return depth.
+ */
+
+size_t binary_tree_depth(const binary_tree t *tree)
+{
+	if (tree)
+	{
+		return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	}
